88bfcea70124efc91d5908daf15b57494ef7878f
LiangMouse <1979139861@qq.com>
2025-12-02 22:25:18 +0800

    feat: new command type

--- app/main.ts
+++ app/main.ts
@@ -1,20 +1,30 @@
+import { existsSync } from "fs";
 import { createInterface } from "readline";
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+const command_list = ["echo", "exit", "type"];
 function prompt() {
   rl.question("$ ", (answer: string) => {
-    // exit
-    if (answer === "exit") {
-      rl.close();
-      return;
+    switch (true) {
+      case answer === "exit":
+        rl.close();
+        return;
+      case answer.startsWith("type"):
+        if (command_list.includes(answer.slice(5))) {
+          console.log(`${answer.slice(5)} is a shell builtin`);
+        } else {
+          console.log("invalid_command: not found");
+        }
+        break;
+      case answer.startsWith("echo"):
+        console.log(answer.slice(5));
+        break;
+      default:
+        console.log(`${answer}: command not found`);
     }
-    // echo
-    if (answer.startsWith("echo")) {
-      console.log(answer.slice(5));
-    } else console.log(`${answer}: command not found`);
     prompt();
   });
 }
